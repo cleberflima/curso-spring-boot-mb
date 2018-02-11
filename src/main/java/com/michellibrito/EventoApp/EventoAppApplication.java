f43f2a93ceace8943e31da48d8edbdd9675ce898
Cleber Fernandes de Lima <cleberflima@hotmail.com>
2018-02-10 23:29:43 -0200

    commit inicial

---
+++ src/main/java/com/michellibrito/EventoApp/EventoAppApplication.java
@@ -0,0 +1,12 @@
+package com.michellibrito.EventoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoAppApplication.class, args);
+	}
+}
